3cbf7a438de86c637d4ced3e1835bfcd42db6783
fitareq <fitareq@gmail.com>
2025-09-09 13:08:02 +0600

    qr history model to save qr history(generated or scanned)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,8 +26,9 @@ android {
         create("release") {
             keyAlias = System.getenv("KEY_ALIAS")
             keyPassword = System.getenv("KEY_PASSWORD")
-            storeFile = file("${rootDir}/app/upload-keystore.jks")
             storePassword = System.getenv("KEYSTORE_PASSWORD")
+            storeFile = file("${rootDir}/app/upload-keystore.jks")
+
         }
     }
 
@@ -46,7 +47,7 @@ android {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("release")
+            //signingConfig = signingConfigs.getByName("release")
         }
     }
 }
